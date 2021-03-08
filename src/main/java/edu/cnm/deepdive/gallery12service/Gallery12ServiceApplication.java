c5f482d246d8355d2fa2161d753dd924a261e505
tnordquist <toddnordquist144@gmail.com>
2021-03-08 11:59:36 -0700

    Initial commit

---
+++ src/main/java/edu/cnm/deepdive/gallery12service/Gallery12ServiceApplication.java
@@ -0,0 +1,13 @@
+package edu.cnm.deepdive.gallery12service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gallery12ServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Gallery12ServiceApplication.class, args);
+  }
+
+}
---
+++ src/test/java/edu/cnm/deepdive/gallery12service/Gallery12ServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cnm.deepdive.gallery12service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gallery12ServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
