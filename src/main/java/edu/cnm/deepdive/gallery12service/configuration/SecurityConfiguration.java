ee634ad07a2ed9c8515d68f8ebcc30791d049b6d
tnordquist <toddnordquist144@gmail.com>
2021-03-08 17:07:00 -0700

    Finished basic auth configuration.

---
+++ src/main/java/edu/cnm/deepdive/gallery12service/configuration/SecurityConfiguration.java
@@ -0,0 +1,53 @@
+package edu.cnm.deepdive.gallery12service.configuration;
+
+import edu.cnm.deepdive.gallery12service.service.UserService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
+import org.springframework.security.oauth2.core.OAuth2TokenValidator;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtClaimNames;
+import org.springframework.security.oauth2.jwt.JwtClaimValidator;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
+import org.springframework.security.oauth2.jwt.JwtValidators;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+  private final UserService userService;
+
+  @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
+  private String issuerUri;
+  @Value("${spring.security.oauth2.resourceserver.jwt.client-id}")
+  private String clientId;
+
+  @Autowired
+  public SecurityConfiguration(UserService userService) {
+    this.userService = userService;
+  }
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.authorizeRequests((auth) -> auth.anyRequest().authenticated())
+        .oauth2ResourceServer().jwt()
+        .jwtAuthenticationConverter(userService);
+  }
+
+  public JwtDecoder jwtDecoder() {
+    NimbusJwtDecoder decoder = (NimbusJwtDecoder) JwtDecoders.fromIssuerLocation(issuerUri);
+    OAuth2TokenValidator<Jwt> audienceValidator =
+        new JwtClaimValidator<List<String>>(JwtClaimNames.AUD, (aud) -> aud.contains(clientId));
+    OAuth2TokenValidator<Jwt> withIssuer = JwtValidators.createDefaultWithIssuer(issuerUri);
+    OAuth2TokenValidator<Jwt> withAudience = new DelegatingOAuth2TokenValidator<Jwt>(withIssuer,
+        audienceValidator);
+    return decoder;
+  }
+}
---
+++ src/main/java/edu/cnm/deepdive/gallery12service/model/dao/UserRepository.java
@@ -0,0 +1,12 @@
+package edu.cnm.deepdive.gallery12service.model.dao;
+
+import edu.cnm.deepdive.gallery12service.model.entity.User;
+import java.util.Optional;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+  Optional<User> findFirstByOauthKey(String oauthKey);
+
+}
